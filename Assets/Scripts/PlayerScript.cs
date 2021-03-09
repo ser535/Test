1b868db5e72151d84544504de80554524b4ebed6
ser535 <sergeykhloptsov@gmail.com>
2021-03-09 20:14:44 +0700

    09.03 привет ваня абрамов я почти доделал финиш и понерфил уровень специально для тебя

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,15 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class PlayerScript : MonoBehaviour
 {
-    public int speed = 5;
-    Rigidbody2D rb;
+    [SerializeField] float startSpeed = 5f;
     [SerializeField] float jumpHeight = 10;
-    [SerializeField] LayerMask groundMask;
     [SerializeField] float radius;
+    [SerializeField] LayerMask groundMask;
+    Rigidbody2D rb;
     bool isGround;
     bool Check = false;
     bool isJump = false;
+    bool isFinish = false;
     float lastPosX;
+    float speed;
 
 
     void Start()
@@ -21,13 +23,14 @@ public class PlayerScript : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         lastPosX = transform.position.x;
 
+        speed = startSpeed;
     }
 
     void Update()
     {
         rb.velocity = new Vector2(speed, rb.velocity.y);
         isGround = Physics2D.OverlapCircle(transform.position, radius, groundMask);
-        if ((Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Space)) && isGround && !isJump)
+        if ((Input.GetKey(KeyCode.Mouse0) || Input.GetKey(KeyCode.Space)) && isGround && !isJump && !isFinish)
         {
             rb.velocity = new Vector2(rb.velocity.x, 0);
             rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
@@ -36,7 +39,7 @@ public class PlayerScript : MonoBehaviour
         }
         if (Check)
         {
-            if (transform.position.x - lastPosX <= 0.005f)
+            if (transform.position.x - lastPosX <= 0.005f && !isFinish)
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
@@ -53,11 +56,21 @@ public class PlayerScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("гг не будет");
         if (collision.gameObject.CompareTag("Death"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            Debug.Log("гг не будет");
+            speed = 0;
+            isFinish = true;
         }
     }
 
